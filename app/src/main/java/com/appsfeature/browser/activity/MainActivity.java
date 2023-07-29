5cde370f9cb01826e7ba0340c2d88c1369d746b4
appsfeature <appsfeature@gmail.com>
2023-07-29 20:18:41 +0530

    Update all versions

--- app/src/main/java/com/appsfeature/browser/activity/MainActivity.java
+++ app/src/main/java/com/appsfeature/browser/activity/MainActivity.java
@@ -2,16 +2,15 @@ package com.appsfeature.browser.activity;
 
 import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Looper;
 
 import com.appsfeature.browser.util.AppConstant;
 import com.browser.BrowserClassUtil;
-import com.browser.BrowserSdk;
 
 
-public class SplashScreen extends Activity {
+public class MainActivity extends Activity {
 	final Context context = this;
 	
 	// Splash screen timer
@@ -24,10 +23,10 @@ public class SplashScreen extends Activity {
 	}
 
 	private void startMainActivity() {
-		new Handler().postDelayed(new Runnable() {
+		new Handler(Looper.myLooper()).postDelayed(new Runnable() {
 			@Override
 			public void run() {
-				BrowserClassUtil.openProfileLink(SplashScreen.this, "Live Tv", AppConstant.BASE_URL);
+				BrowserClassUtil.openProfileLink(MainActivity.this, "Live Tv", AppConstant.BASE_URL);
 //				startActivity(new Intent(SplashScreen.this, BrowserCustomActivity.class));
 				finish();
 			}
